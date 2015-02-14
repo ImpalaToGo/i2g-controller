19d463c16a5547102d4eab9b5d078d08e8efa247
Robert Navado <sbornava@gmail.com>
2015-02-14 14:34:21 +0200

    Split configuration.
    Added 3 new configurations:
    - instance.config       Configuration for AMI and image size
    - instance_key.config   Configuration for private key and it's path
    - s3.config             Configuration for access credentials to S3
    
    Rewrite Configure script for controller accordingly
    Removes resize template as redundant

--- cluster/configure.sh
+++ cluster/configure.sh
@@ -2,19 +2,20 @@ echo "Configuring ImpalaToGo"
 
 read -p "Please enter your access key " ACCESS_KEY
 read -p "Please enter your secret key " SECRET_KEY
-read -p "Please enter your default s3 backet" S3_BACKET
+read -p "Please enter your default s3 backet " S3_BUCKET
+read -p "Please enter your key name " KEY_NAME
+read -p "please enter you key file name " KEY_FILE_NAME
 
 
-read -p "Is it master ? y/n " IsMaster
-
-if [ "$IsMaster" == "n" ]; then
-	read -p "Please enter DNS of Master" MASTER_DNS
-else
-	MASTER_DNS=localhost
-fi
-
-#get script path
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
+S3_CONFIG=$SCRIPTPATH/s3.config
+echo ACCESS_KEY=$ACCESS_KEY >$S3_CONFIG
+echo SECRET_KEY=$SECRET_KEY >>$S3_CONFIG
+echo S3_BUCKET=$S3_BUCKET >>$S3_CONFIG
 
-$SCRIPTPATH/attachToCluster.sh $ACCESS_KEY $SECRET_KEY $MASTER_DNS $S3_BACKET
+INSTANCE_KEY_CONFIG=$SCRIPTPATH/instance_key.config
+echo KEY_NAME=$KEY_NAME >$INSTANCE_KEY_CONFIG
+echo PRIVATE_KEY=$KEY_FILE_NAME >>$INSTANCE_KEY_CONFIG
+
+echo configuration finished. Please make sure that your key file $KEY_FILE_NAME is sitting in /home/ec2-user/cluster directory
